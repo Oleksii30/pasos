8102147d09a4a89be34fb9cfe09fa2f8f06f4494
Oleksii30 <45796003+Oleksii30@users.noreply.github.com>
2020-03-04 00:09:16 +0200

    Update check-auth.js

--- midelware/check-auth.js
+++ midelware/check-auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken')
+const forToken = process.env.forToken
 
 module.exports = (req,res,next)=>{
     try{
         const token = req.headers.authorization.split(" ")[1]
-        jwt.verify(token, "jjkjhhfdxdcbbyyresddjllokmjhfd")
+        jwt.verify(token, forToken)
         next()
     }catch (error){
         res.status(401).json({message: "Auth failed"})
